03951c2c2ca0f0cc8a65ae6545ac1460bceb59f4
gerardo <gerardo.parra139@gmail.com>
2024-05-21 17:28:21 +0200

    fix test_resize

--- processing/blur.py
+++ processing/blur.py
@@ -3,8 +3,8 @@ import skimage as ski
 from matplotlib import  pyplot as plt
 import os
 
-def blur_normalized_box(image,ksize):
+def normalized_box(image,ksize):
     return cv2.blur(image,ksize)
 
-def blur_gaussian(image,ksize,sigmaX):
+def gaussian(image,ksize,sigmaX):
     return cv2.GaussianBlur(image,ksize,sigmaX)
