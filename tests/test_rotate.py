43274ba4458e1127f71b41c07eff319efa6459e8
bnosnehpets-neuroscience <157141385+bnosnehpets-neuroscience@users.noreply.github.com>
2024-05-21 16:41:13 +0200

    minor changes to test_rotate.py

--- tests/test_rotate.py
+++ tests/test_rotate.py
@@ -4,8 +4,8 @@ import cv2
 class ImageRotateTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.image_metadata = load_test_image_metadata.load_test_image_metadata()
+        self.image_metadata = demodata.load_test_image_metadata.load_test_image_metadata()
+        self.image = dataio.load_image.load_image(self.image_metadata['path'])
 
     def test_image_rotate_360(self):
-        image = load_image.load_image(self.image_metadata['path'])
-        self.assertEqual(image, 360)
+        self.assertEqual(self.image, 360)
